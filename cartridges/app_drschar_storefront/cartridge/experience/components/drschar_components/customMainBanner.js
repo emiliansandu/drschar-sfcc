422575996d6d0d5659e94f35c6d977f6ba195e28
ocorderouv <ocordero@unitedvirtualities.com>
2021-06-02 11:48:44 -0600

    [DRS-155] Custom main banner component

---
+++ cartridges/app_drschar_storefront/cartridge/experience/components/drschar_components/customMainBanner.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var Template = require('dw/util/Template');
+var HashMap = require('dw/util/HashMap');
+var URLUtils = require('dw/web/URLUtils');
+var ImageTransformation = require('*/cartridge/experience/utilities/ImageTransformation.js');
+
+/**
+ * Render logic for the storefront.MainBanner component
+ * @param {dw.experience.ComponentScriptContext} context The Component script context object.
+ * @param {dw.util.Map} [modelIn] Additional model values created by another cartridge. This will not be passed in by Commcerce Cloud Plattform.
+ *
+ * @returns {string} The markup to be displayed
+ */
+module.exports.render = function (context, modelIn) {
+    var model = new HashMap();
+    var content = context.content;
+
+    model.heading = content.heading;
+    model.image = ImageTransformation.getScaledImage(content.image);
+    model.categoryLink = "";
+
+    if(content.categoryLink != undefined){
+        model.categoryLink = URLUtils.url('Search-Show', 'cgid', content.categoryLink.getID()).toString();
+    }
+
+    model.color = content.color;
+    model.position = content.position;
+
+    return new Template('experience/components/drschar_components/customMainBanner').render(model).text;
+};
