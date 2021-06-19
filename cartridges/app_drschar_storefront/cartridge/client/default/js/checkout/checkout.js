e70528b652e136b494b97c6ec34b0b8925b5f494
ocorderouv <ocordero@unitedvirtualities.com>
2021-06-18 18:08:23 -0600

    [DRS-182] Including and conditioning the include files

---
+++ cartridges/app_drschar_storefront/cartridge/client/default/js/checkout/checkout.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var shippingHelpers = require('base/checkout/shipping');
+
+var billingHelpers = require('base/checkout/billing');
+
+var summaryHelpers = require('base/checkout/summary');
+
+var billing = require('./billing');
+
+var adyenCheckout = require('../adyenCheckout');
+
+module.exports = {
+  updateCheckoutView: function updateCheckoutView() {
+    $('body').on('checkout:updateCheckoutView', function (e, data) {
+      shippingHelpers.methods.updateMultiShipInformation(data.order);
+      summaryHelpers.updateTotals(data.order.totals);
+      data.order.shipping.forEach(function (shipping) {
+        shippingHelpers.methods.updateShippingInformation(shipping, data.order, data.customer, data.options);
+      });
+      var currentStage = location.search.substring( // eslint-disable-line no-restricted-globals
+      location.search.indexOf('=') + 1 // eslint-disable-line no-restricted-globals
+      );
+
+      if (currentStage === 'shipping') {
+        adyenCheckout.methods.renderGenericComponent();
+      }
+
+      billingHelpers.methods.updateBillingInformation(data.order, data.customer, data.options);
+      billing.methods.updatePaymentInformation(data.order, data.options);
+      summaryHelpers.updateOrderProductSummaryInformation(data.order, data.options);
+    });
+  }
+};
