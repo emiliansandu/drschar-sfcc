726f6d8be30ea95492663a979f2d684a6d89c4be
ocorderouv <ocordero@unitedvirtualities.com>
2021-06-21 14:03:51 -0600

    [DRS-190] Adding the rest of the missing endpoints on Search Controller

--- cartridges/app_drschar_storefront/cartridge/controllers/Search.js
+++ cartridges/app_drschar_storefront/cartridge/controllers/Search.js
@@ -55,6 +55,80 @@ var pageMetaData = require('*/cartridge/scripts/middleware/pageMetaData');
 });
 
 /**
+ * Search-Refinebar : The endpoint Search-Refinebar render the refinement bar on product list page, PLP (i.e. the search result page and category listing page)
+ * @name Base/Search-Refinebar
+ * @function
+ * @memberof Search
+ * @param {middleware} - cache.applyDefaultCache
+ * @param {querystringparameter} - q - The search string (when submit product search)
+ * @param {querystringparameter} - cgid - category ID (when loading category list page)
+ * @param {category} - non-sensitive
+ * @param {renders} - isml
+ * @param {serverfunction} - get
+ */
+ server.get('Refinebar', cache.applyDefaultCache, function (req, res, next) {
+    var CatalogMgr = require('dw/catalog/CatalogMgr');
+    var ProductSearchModel = require('dw/catalog/ProductSearchModel');
+    var ProductSearch = require('*/cartridge/models/search/productSearch');
+    var searchHelper = require('*/cartridge/scripts/helpers/searchHelpers');
+
+    var apiProductSearch = new ProductSearchModel();
+    apiProductSearch = searchHelper.setupSearch(apiProductSearch, req.querystring, req.httpParameterMap);
+    apiProductSearch.search();
+    var productSearch = new ProductSearch(
+        apiProductSearch,
+        req.querystring,
+        req.querystring.srule,
+        CatalogMgr.getSortingOptions(),
+        CatalogMgr.getSiteCatalog().getRoot()
+    );
+    res.render('/search/searchRefineBar', {
+        productSearch: productSearch,
+        querystring: req.querystring
+    });
+
+    next();
+}, pageMetaData.computedPageMetaData);
+
+/**
+ * Search-ShowAjax : This endpoint is called when a shopper click on any of the refinement eg. color, size, categories
+ * @name Base/Search-ShowAjax
+ * @function
+ * @memberof Search
+ * @param {middleware} - cache.applyShortPromotionSensitiveCache
+ * @param {middleware} - consentTracking.consent
+ * @param {querystringparameter} - cgid - Category ID
+ * @param {querystringparameter} - q - query string a shopper is searching for
+ * @param {querystringparameter} - prefn1, prefn2 ... prefn(n) - Names of the selected preferences e.g. refinementColor. These will be added to the query parameters only when refinements are selected
+ * @param {querystringparameter} - prefv1, prefv2 ... prefv(n) - Values of the selected preferences e.g. Blue. These will be added to the query parameters only when refinements are selected
+ * @param {querystringparameter} - pmin - preference for minimum amount
+ * @param {querystringparameter} - pmax - preference for maximum amount
+ * @param {querystringparameter} - page
+ * @param {querystringparameter} - selectedUrl - The URL generated with the query parameters included
+ * @param {category} - non-sensitive
+ * @param {serverfunction} - get
+ */
+ server.get('ShowAjax', cache.applyShortPromotionSensitiveCache, consentTracking.consent, function (req, res, next) {
+    var searchHelper = require('*/cartridge/scripts/helpers/searchHelpers');
+
+    var result = searchHelper.search(req, res);
+
+    if (result.searchRedirect) {
+        res.redirect(result.searchRedirect);
+        return next();
+    }
+
+    res.render('search/searchResultsNoDecorator', {
+        productSearch: result.productSearch,
+        maxSlots: result.maxSlots,
+        reportingURLs: result.reportingURLs,
+        refineurl: result.refineurl
+    });
+
+    return next();
+}, pageMetaData.computedPageMetaData);
+
+/**
  * Search-Show : This endpoint is called when a shopper type a query string in the search box
  * @name Base/Search-Show
  * @function
@@ -129,5 +203,29 @@ server.get('Show', cache.applyShortPromotionSensitiveCache, consentTracking.cons
 
     return next();
 }, pageMetaData.computedPageMetaData);
+
+/**
+ * Search-Content : This endpoint is called when a shopper search for something under articles by clicking on the articles tab next to products on Search result page
+ * @name Base/Search-Content
+ * @function
+ * @memberof Search
+ * @param {middleware} - cache.applyDefaultCache
+ * @param {middleware} - consentTracking.consent
+ * @param {querystringparameter} - q - the query string a shopper is searching for
+ * @param {querystringparameter} - startingPage - The starting page to display in the case there are multiple pages returned
+ * @param {category} - non-sensitive
+ * @param {renders} - isml
+ * @param {serverfunction} - get
+ */
+ server.get('Content', cache.applyDefaultCache, consentTracking.consent, function (req, res, next) {
+    var searchHelper = require('*/cartridge/scripts/helpers/searchHelpers');
+
+    var contentSearch = searchHelper.setupContentSearch(req.querystring);
+    res.render('/search/contentGrid', {
+        contentSearch: contentSearch
+    });
+    next();
+});
+
 
 module.exports = server.exports();
