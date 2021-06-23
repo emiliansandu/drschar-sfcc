a583ff2517000f1e8ea4b3cf32823a2767698a60
ocorderouv <ocordero@unitedvirtualities.com>
2021-06-23 10:19:13 -0600

    [DRS-182] Add files from base

--- cartridges/app_drschar_storefront/cartridge/client/default/js/checkout.js
+++ cartridges/app_drschar_storefront/cartridge/client/default/js/checkout.js
@@ -1,45 +1,7 @@
-"use strict";
+'use strict';
 
-var processInclude = require('base/util');
-
-var adyenCheckout = require('./adyenCheckout');
+var processInclude = require('./util');
 
 $(document).ready(function () {
-  
-  $('#selectedPaymentOption').val($('.payment-options .nav-item .active').parent().attr('data-method-id'));
-  var selectedPayment = $('#selectedPaymentOption').val();
-  if(selectedPayment == 'AdyenComponent'){
-    initFrontAdyen();
-  }
-  processInclude(require('base/checkout/checkout'));
+    processInclude(require('./checkout/checkout'));
 });
-$('.payment-options .nav-link').click(function () {
-  $('#selectedPaymentOption').val($(this).parent().attr('data-method-id'));
-  var selectedPayment = $('#selectedPaymentOption').val();
-  if(selectedPayment == 'AdyenComponent'){
-    initFrontAdyen();
-  }
-  processInclude(require('base/checkout/checkout'));
-  console.log('change');
-});
-
-function initFrontAdyen() {
-  // eslint-disable-line
-  var name = 'paymentError';
-  var error = new RegExp("[?&]".concat(encodeURIComponent(name), "=([^&]*)")).exec(location.search // eslint-disable-line no-restricted-globals
-  );
-  var paymentStage = new RegExp('[?&]stage=payment([^&]*)').exec(location.search // eslint-disable-line no-restricted-globals
-  );
-
-  if (error || paymentStage) {
-    if (error) {
-      $('.error-message').show();
-      $('.error-message-text').text(decodeURIComponent(error[1]));
-    }
-
-    adyenCheckout.methods.renderGenericComponent();
-  }
-
-  processInclude(require('./checkout/billing'));
-  processInclude(require('./checkout/checkout'));
-}
