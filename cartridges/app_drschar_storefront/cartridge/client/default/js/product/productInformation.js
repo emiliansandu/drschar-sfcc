760ace4ee66f25e4abb239246d5e686c84ec67d1
ocorderouv <ocordero@unitedvirtualities.com>
2021-06-30 13:35:17 -0600

    [DRS-204] Dinamic square image

--- cartridges/app_drschar_storefront/cartridge/client/default/js/product/productInformation.js
+++ cartridges/app_drschar_storefront/cartridge/client/default/js/product/productInformation.js
@@ -9,4 +9,14 @@ $(document).ready(function () {
         scrollAnimate($('.description-and-detail'));
     });
 
+    //Dinamic resize image height - Square image
+    var elem = $(".img-fluid, .img-thumbnail")[0];
+  
+    let resizeObserver = new ResizeObserver(() => {
+        var cw = $('.img-fluid, .img-thumbnail').width();
+        $('.img-fluid, .img-thumbnail').css({'height':cw+'px'});
+    });
+
+    resizeObserver.observe(elem);
+
 });
