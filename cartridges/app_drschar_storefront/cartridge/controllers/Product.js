e0101da7e24564230323f6f2ccf51d9d242030b9
Mike <jmorodriguez@unitedvirtualities.com>
2021-07-01 21:12:21 -0600

    [DRS-213] Fix for empty customObject

--- cartridges/app_drschar_storefront/cartridge/controllers/Product.js
+++ cartridges/app_drschar_storefront/cartridge/controllers/Product.js
@@ -40,6 +40,9 @@ server.get('Show', cache.applyPromotionSensitiveCache, consentTracking.consent, 
     var showProductPageHelperResult = productHelper.showProductPage(req.querystring, req.pageMetaData);
     var productType = showProductPageHelperResult.product.productType;
     var NutritionalFactsObj = CustomObjectMgr.queryCustomObject('NutritionFacts','custom.productID = {0}', fullProduct.ID);
+    if (NutritionalFactsObj == null) {
+        NutritionalFactsObj = {custom: null};
+    }
     if (!showProductPageHelperResult.product.online && productType !== 'set' && productType !== 'bundle') {
         res.setStatusCode(404);
         res.render('error/notFound');
