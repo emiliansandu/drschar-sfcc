391c6a210e1d8b39e044e181b268e4a11d1a7428
Jose Luis Herrera Jasso <jherrera@unitedvirtualities>
2021-07-21 15:54:37 -0600

    [DRS-268] Forgotten fixes

--- cartridges/app_drschar_storefront/cartridge/client/default/js/instagramFeed.js
+++ cartridges/app_drschar_storefront/cartridge/client/default/js/instagramFeed.js
@@ -2,9 +2,11 @@
 
 $(function () {
    var div_width = $(".product-tile").width()+'px';
+   $(".product-tile").css({'min-height': div_width });
     $(".tile-image-background").css({'height': div_width }); 
+    $(".image-container").css({'height': div_width });
+    $(".overlay").css({'height': div_width }); 
     $(window).on("resize",(function() {
-     
         var div_width = $(".product-tile").width()+'px';
         $(".product-tile").css({'min-height': div_width });
         $(".tile-image-background").css({'height': div_width }); 
