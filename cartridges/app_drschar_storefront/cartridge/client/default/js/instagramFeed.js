93609d228cef2760c3f67bd5cde7079fa98c9501
Jose Luis Herrera Jasso <jherrera@unitedvirtualities>
2021-07-21 17:05:58 -0600

    [DRS-268] Refactor of code to do the same but in function instead of typing the same code for every event

--- cartridges/app_drschar_storefront/cartridge/client/default/js/instagramFeed.js
+++ cartridges/app_drschar_storefront/cartridge/client/default/js/instagramFeed.js
@@ -1,16 +1,17 @@
 "use strict";
 
 $(function () {
-   var div_width = $(".product-tile").width()+'px';
+  instagramTileResize();
+    $(window).on("resize",(function() {
+      instagramTileResize();
+      }));    
+});
+function instagramTileResize(){
+
+  var div_width = $(".product-tile").width()+'px';
    $(".product-tile").css({'min-height': div_width });
     $(".tile-image-background").css({'height': div_width }); 
     $(".image-container").css({'height': div_width });
-    $(".overlay").css({'height': div_width }); 
-    $(window).on("resize",(function() {
-        var div_width = $(".product-tile").width()+'px';
-        $(".product-tile").css({'min-height': div_width });
-        $(".tile-image-background").css({'height': div_width }); 
-        $(".image-container").css({'height': div_width });
-        $(".overlay").css({'height': div_width });
-      }));    
-});
+    $(".overlay").css({'height': div_width });
+  
+}
