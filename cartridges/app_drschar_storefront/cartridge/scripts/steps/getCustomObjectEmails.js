4d4936cdce46613211c20a1d11c54c72e55620f1
Jose Luis Herrera Jasso <jherrera@unitedvirtualities>
2021-07-28 13:08:46 -0600

    [DRS-166] Created custom.getCustomObjectEmails job step to be used on job marketing_Cloud_Signup_Emails_Page_Designer_Component_Job as step-id exportXMLEmailFile

---
+++ cartridges/app_drschar_storefront/cartridge/scripts/steps/getCustomObjectEmails.js
@@ -0,0 +1,58 @@
+'use strict';
+
+//var server = require('server');
+var Logger = require('dw/system/Logger');
+var CustomObjectMgr = require('dw/object/CustomObjectMgr');
+var Transaction = require('dw/system/Transaction');
+var instaHelper = require('~/cartridge/scripts/helpers/instagramHelper');
+
+/**
+ * @function getCOs
+ * @description Function that gets a custom object for a CO type and key attribute passed as a parameter.
+ *
+ * @param {Object} parameters Represents the parameters defined in the steptypes.json file
+ */
+module.exports = {
+    getCOmarketingCustomer : function getCOmarketingCustomer(parameters) {
+        var XMLStreamWriter = require('dw/io/XMLStreamWriter');
+        const File = require('dw/io/File');
+        const FileWriter = require('dw/io/FileWriter');
+        
+        var customObjectInstanceList = CustomObjectMgr.getAllCustomObjects(parameters.CustomObjectType);
+        var CustomObjectCount=customObjectInstanceList.getCount();
+        var CustomObjectData=customObjectInstanceList.asList(0, CustomObjectCount);
+        // Save XML on: /on/demandware.servlet/webdav/Sites/Impex/MarketingCloud/export-customer_emails_page_designer_marketing_cloud.xml 
+        var impexPath = File.IMPEX + File.SEPARATOR + 'Impex' + File.SEPARATOR + 'src' + File.SEPARATOR + 'IMPEX' + File.SEPARATOR + parameters.TargetFolder;
+        var impexPathFile = File.IMPEX + File.SEPARATOR + 'Impex' + File.SEPARATOR + 'src' + File.SEPARATOR + 'IMPEX' + File.SEPARATOR + parameters.TargetFolder + File.SEPARATOR + parameters.Filename;
+        var customDir = new File(impexPath);
+        var file = new File(impexPathFile);
+        var existsDir=customDir.exists();
+    if(existsDir==false){
+        customDir.mkdir();
+    }
+        var fileWriter : FileWriter = new FileWriter(file, "UTF-8");
+        var xsw : XMLStreamWriter = new XMLStreamWriter(fileWriter);
+
+        xsw.writeStartDocument();
+            xsw.writeCharacters( "\n" );
+                xsw.writeStartElement("customerEmails");
+                    xsw.writeCharacters( "\n" );
+                    for(var i=0; i<CustomObjectData.length; i++){
+                        xsw.writeStartElement("email");
+                        xsw.writeAttribute("id", "email_"+i);
+                            xsw.writeCharacters( "\n" ); 
+                                xsw.writeCharacters(CustomObjectData[i].custom.emailAddress);
+                                    xsw.writeCharacters( "\n" );
+                        xsw.writeEndElement();
+                            xsw.writeCharacters( "\n" );
+                         }  
+                    
+                xsw.writeEndElement();
+            xsw.writeCharacters( "\n" );
+        xsw.writeEndDocument();
+       
+        xsw.close();
+        fileWriter.close();
+            
+    }
+        };
