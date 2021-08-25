17bfe925ce07160138a38a481148a40583270bb2
ocorderouv <ocordero@unitedvirtualities.com>
2021-08-25 11:26:33 -0600

    [DRS-99] Validate the image of the item from the order

--- cartridges/app_drschar_storefront/cartridge/scripts/checkout/checkoutHelpers.js
+++ cartridges/app_drschar_storefront/cartridge/scripts/checkout/checkoutHelpers.js
@@ -547,10 +547,12 @@ function sendConfirmationEmail(order, locale, host, paymentObject, contentAsset,
 
     var orderModel = new OrderModel(order, { countryCode: currentLocale.country, containerView: 'order' });
     for(var i=0; i<orderModel.items.items.length; i++){
-        var imageURL=orderModel.items.items[i].images.small[0].url;
-        if (imageURL.indexOf('?sw=140&sh=140') == -1) {
-            imageURL = 'https://' + host + imageURL;
-            orderModel.items.items[i].images.small[0].url = imageURL;
+        if(orderModel.items.items[i].images.small[0].title != 'No Image'){
+            var imageURL=imageURL = orderModel.items.items[i].images.small[0].url;
+            if (imageURL.indexOf('?sw=140&sh=140') == -1) {
+                imageURL = 'https://' + host + imageURL;
+                orderModel.items.items[i].images.small[0].url = imageURL;
+            }
         }
     }
     var estimatedArrival = orderModel.shipping[0].selectedShippingMethod.estimatedArrivalTime.replace(" Business Days", "");
@@ -630,10 +632,12 @@ function sendConfirmationEmail(order, locale, host, paymentObject, contentAsset,
     var orderModel = new OrderModel(order, { countryCode: currentLocale.country, containerView: 'order' });
 
     for(var i=0; i<orderModel.items.items.length; i++){
-        var imageURL=orderModel.items.items[i].images.small[0].url;
-        if (imageURL.indexOf('?sw=140&sh=140') == -1) {
-            imageURL = 'https://' + host + imageURL;
-            orderModel.items.items[i].images.small[0].url = imageURL;
+        if(orderModel.items.items[i].images.small[0].title != 'No Image'){
+            var imageURL=orderModel.items.items[i].images.small[0].url;
+            if (imageURL.indexOf('?sw=140&sh=140') == -1) {
+                imageURL = 'https://' + host + imageURL;
+                orderModel.items.items[i].images.small[0].url = imageURL;
+            }
         }
     }
 
