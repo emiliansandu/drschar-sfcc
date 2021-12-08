b71a36740d195467f54708cb1e80362441d46efe
ocorderouv <ocordero@unitedvirtualities.com>
2021-12-08 09:50:49 -0700

    [DRS-362] Override function from helper base to custom cartridge

--- cartridges/app_drschar_storefront/cartridge/scripts/helpers/productHelpers.js
+++ cartridges/app_drschar_storefront/cartridge/scripts/helpers/productHelpers.js
@@ -123,4 +123,40 @@ PHelper.verifyAvailability = function verifyAvailability(object){
     return status;
 }
 
+PHelper.showProductPage = function showProductPage(querystring, reqPageMetaData) {
+    var URLUtils = require('dw/web/URLUtils');
+    var ProductFactory = require('*/cartridge/scripts/factories/product');
+    var pageMetaHelper = require('*/cartridge/scripts/helpers/pageMetaHelper');
+
+    var params = querystring;
+    var product = ProductFactory.get(params);
+    var addToCartUrl = URLUtils.url('Cart-AddProduct');
+    var canonicalUrl = URLUtils.url('Product-Show', 'pid', product.id);
+    var breadcrumbs = getAllBreadcrumbs(null, product.id, []).reverse();
+
+    var template = 'product/productDetails';
+
+    if (product.productType === 'bundle' && !product.template) {
+        template = 'product/bundleDetails';
+    } else if (product.productType === 'set' && !product.template) {
+        template = 'product/setDetails';
+    } else if (product.template) {
+        template = product.template;
+    }
+
+    pageMetaHelper.setPageMetaData(reqPageMetaData, product);
+    pageMetaHelper.setPageMetaTags(reqPageMetaData, product);
+    var schemaData = require('*/cartridge/scripts/helpers/structuredDataHelper').getProductSchema(product);
+
+    return {
+        template: template,
+        product: product,
+        addToCartUrl: addToCartUrl,
+        resources: getResources(),
+        breadcrumbs: breadcrumbs,
+        canonicalUrl: canonicalUrl,
+        schemaData: schemaData
+    };
+}
+
 module.exports = PHelper;
