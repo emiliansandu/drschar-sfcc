600ca7ec979bfd025029adb898d33c8eee6de955
Jose Luis Herrera Jasso <jherrera@unitedvirtualities.com>
2021-12-13 14:23:26 -0700

    [DRS-351] Added Parameters and default values to steptype custom.getInstagramContent on steptypes.json and finished on creating the new step type script which will be the one that handle the request to the API using the instagramHelper function and save the API request values obtained from that function to custom object instagramContent

--- cartridges/app_drschar_storefront/cartridge/scripts/steps/InstagramContent.js
+++ cartridges/app_drschar_storefront/cartridge/scripts/steps/InstagramContent.js
@@ -19,22 +19,45 @@ module.exports = {
         var CustomObjectData = CustomObjectInstance.asList(0, CustomObjectCount);
         var tokenKey = CustomObjectData[0].custom.tokenKey;
 
-        if (tokenKey) {
-            var apiPath = "me/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url&limit=8";
+        if (parameters.CustomObjectApiPath && tokenKey) {
+            var apiPath = parameters.CustomObjectApiPath+"&limit=8";
             var instaFeed = instaHelper.getFeed(apiPath, tokenKey);
-            if (instaFeed != null) {
-                saveInstagramContent(instaFeed);
+            if (instaFeed != null) {             
+                for(var i=0; i<instaFeed.data.length; i++){         
+                saveInstagramContent(instaFeed, i);
             }
-            infoLog.info("The data is saved in a custom object");
+            var infoLog = Logger.getLogger("info-insgDataSave", "customjobInstagramTokenInfo");
+            infoLog.info("The data has been saved on instagramContent custom object");   
+            }
+    
         };
     }
 
 
 };
 
-function saveInstagramContent(instaFeed) {
-    var CustomObjectInstance = CustomObjectMgr.getAllCustomObjects('instagramContent');
-    var CustomObjectCount = CustomObjectInstance.getCount();
-    var CustomObjectData = CustomObjectInstance.asList(0, CustomObjectCount);
-    CustomObjectData[0].custom.data = instaFeed;
+function saveInstagramContent(instaFeed, i) {  
+          
+   try {
+    var customObjectKeyValueExists = CustomObjectMgr.getCustomObject('instagramContent', i);
+    if (!customObjectKeyValueExists) {
+       var CustomObject = CustomObjectMgr.createCustomObject('instagramContent', i);
+       CustomObject.custom.ID=instaFeed.data[i].id;
+       CustomObject.custom.caption=instaFeed.data[i].caption;
+       CustomObject.custom.mediaType=instaFeed.data[i].media_type;
+       CustomObject.custom.mediaURL=instaFeed.data[i].media_url;
+       CustomObject.custom.permalink=instaFeed.data[i].permalink;
+       CustomObject.custom.thumbnailURL=instaFeed.data[i].thumbnail_url;
+    } else{
+       customObjectKeyValueExists.custom.ID=instaFeed.data[i].id;
+       customObjectKeyValueExists.custom.caption=instaFeed.data[i].caption;
+       customObjectKeyValueExists.custom.mediaType=instaFeed.data[i].media_type;
+       customObjectKeyValueExists.custom.mediaURL=instaFeed.data[i].media_url;
+       customObjectKeyValueExists.custom.permalink=instaFeed.data[i].permalink;
+       customObjectKeyValueExists.custom.thumbnailURL=instaFeed.data[i].thumbnail_url;
+    }    
+    } catch (e) {
+        var errorLog = Logger.getLogger("error-insgDataSave", "customjobInstagramTokenInfo");
+            errorLog.error("The data could not been saved in instagramContent custom object");
+      }
 }
