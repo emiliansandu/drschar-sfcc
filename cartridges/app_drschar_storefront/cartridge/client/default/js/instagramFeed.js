0957fb49344c51473af055934fe508941535b311
Jose Luis Herrera Jasso <jherrera@unitedvirtualities.com>
2021-12-13 14:32:25 -0700

    [DRS-351] Fixed a renderization bug caused by the way the data is being obtained wich is a fatser way which makes the rezise function to not work properly which was causing the component not being loaded on first time the page loads but only after a manual resize or refresh of the page

--- cartridges/app_drschar_storefront/cartridge/client/default/js/instagramFeed.js
+++ cartridges/app_drschar_storefront/cartridge/client/default/js/instagramFeed.js
@@ -1,17 +1,19 @@
 "use strict";
 
 $(function () {
-  instagramTileResize();
+  $(window).on('load', function() {
+    instagramTileResize();
+   }); 
     $(window).on("resize",(function() {
       instagramTileResize();
       }));    
 });
 function instagramTileResize(){
-
+  $(".experience-drschar_components-instagramFeed").css("display", "block");
   var div_width = $(".product-tile").width()+'px';
    $(".product-tile").css({'min-height': div_width });
     $(".tile-image-background").css({'height': div_width }); 
     $(".image-container").css({'height': div_width });
     $(".overlay").css({'height': div_width });
-  
+
 }
