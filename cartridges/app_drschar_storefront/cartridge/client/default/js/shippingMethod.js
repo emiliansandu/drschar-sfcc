15c3363963d2f77bf444a69e3e507b8555aff0d7
Jose Luis Herrera Jasso <jherrera@unitedvirtualities.com>
2022-01-10 22:38:15 -0700

    [DRS-367] Added shipping discount data to be displayed as applied on shipping methods lists  when there are shipping discounts to be applied also included the aritmetical operation of the applied discount and result display on template

--- cartridges/app_drschar_storefront/cartridge/client/default/js/shippingMethod.js
+++ cartridges/app_drschar_storefront/cartridge/client/default/js/shippingMethod.js
@@ -4,6 +4,15 @@ $(document).ajaxStop(function() {
 
 $(document).ready(function() {
     checkShippingMethodList();
+
+    $('.shipping-method-list').find('.shipping-method-pricing > del > .shipping-cost').each(function(){
+        var shippingCost = $(this).text();
+        shippingCost = shippingCost.split('$');
+        shippingCost = shippingCost[1];
+        var shippingLevelDiscountTotal=$(this).data("shipping-level-discount-total");
+        var totalWithAppliedPromotion=shippingCost-shippingLevelDiscountTotal;
+        $(this).parent().parent().find(".totalWithAppliedPromotion").text(totalWithAppliedPromotion);
+   });
 });
 
 
